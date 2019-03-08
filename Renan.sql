39addf02c3d982452a8184aab06ae0bc856b78d8
Renan Groth <31290593+renangroth@users.noreply.github.com>
2019-03-08 11:04:46 -0300

    Update and rename PrimeiraAula to Renan.sql

---
+++ Renan.sql
@@ -0,0 +1,54 @@
+//sudo -u postgres psql postgres
+
+
+CREATE or REPLACE FUNCTION somefunc(int valor) RETURNS int AS $$
+BEGIN
+	valor = valor + 1;
+
+END; $$
+LANGUAGE plpgsql;
+
+
+
+
+
+create table dados (name varchar(50), salary float, id int);
+INSERT INTO dados values ('XXX', 100,1);
+
+CREATE FUNCTION selecionar(p_itemno int)
+RETURNS TABLE(name varchar(50), salary float) AS $$
+begin
+	RETURN QUERY SELECT s.name, s.salary from dados AS s
+		WHERE s.id = p_itemno;
+end;
+$$ LANGUAGE plpgsql;
+
+
+
+
+create table dado ( id int , name varchar(50), age int ,
+address varchar(50), salary float );
+insert into dado values (1,'paulo',32, 'são miguel', 2000);
+insert into dado values (2,'joao',22, 'chapeco', 2300);
+insert into dado values (3,'maria',62, 'chapeco', 5300);
+insert into dado values (4,'pedro',42, 'chapeco', 1300);
+
+
+
+create or replace function numero1()
+returns boolean as $$
+declare
+	linha record;
+begin
+	for linha IN select dado.id from dado where dado.id >= 0 
+	loop
+		update dado set salary = salary * 1.1 where dado.id = linha.id;
+
+	end loop;
+
+	return FOUND;
+end;
+$$ language plpgsql;
+
+
+select * from dado;
